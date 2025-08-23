96f83e94d5825da2413042424cf38cf6b68bf8df
Isabela (Dev Local) <isabelalinharessilva@gmail.com>
2025-08-23 20:21:14 -0300

    mini lojinha e uso de funções

--- application/Store.java
+++ application/Store.java
@@ -1,5 +1,38 @@
 package application;
 
+import java.util.Scanner;
+
+import entities.Products;
+
 public class Store {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Products item = new Products();
+        System.out.println("Enter product data: ");
+        System.out.print("Name: ");
+        item.name = sc.nextLine();
+        System.out.print("Price: ");
+        item.price = sc.nextDouble();
+        System.out.print("Quantity in stock: ");
+        item.stock = sc.nextInt();
+        //print
+        System.out.print("Product ");
+        item.TotalValueInStock();
+
+        System.out.println("Enter the number of products to be added in stock: ");
+        item.plus = sc.nextInt();
+        System.out.println("Update the number of products to be removed from stock: ");
+        item.remove = sc.nextInt();
+
+        System.out.print("Update ");
+        item.AddProducts();
+        item.TotalValueInStock();
+        System.out.print("Update ");
+        item.RemoveProducts();
+        item.TotalValueInStock();
+
+        sc.close();
+    }
     
+
 }
--- entities/Products.java
+++ entities/Products.java
@@ -1,5 +1,26 @@
 package entities;
 
 public class Products {
-    
+    public String name;
+    public double price;
+    public int stock;
+    public int plus;
+    public int remove;
+
+    public double Total() {
+        return price *stock;
+    }
+
+    public void TotalValueInStock(){
+        System.out.printf("data: %s, $ %.2f, %d units, Total: $ %.2f\n",name,price,stock,Total());
+    }
+
+    public int AddProducts(){
+        return stock += plus;
+    }
+
+    public int RemoveProducts(){
+        return stock -=remove;
+    }
+
 }
