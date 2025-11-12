3c9371215070aaf5537ce35be83ca9564188c2c7
Isabela (Dev Local) <isabelalinharessilva@gmail.com>
2025-11-12 14:28:45 -0300

    uso de try catch

---
+++ tratamento_de_excecoes/Main.java
@@ -0,0 +1,37 @@
+package tratamento_de_excecoes;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        method1();
+        System.out.println("End of program");
+        
+    }
+
+    public static void method1() {
+        System.out.println("***METHOD 1 START***");
+        method2();
+        System.out.println("***METHOD 1 END ***");
+    }
+
+    public static void method2() {
+        System.out.println("***METHOD 2 START***");
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            String[] vect = sc.nextLine().split(" ");
+            int position = sc.nextInt();
+            System.out.println(vect[position]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Invalid poisition!");
+            e.printStackTrace(); // imprime na tela o rastreamento do stack
+        }
+        catch(InputMismatchException e){
+            System.out.println("Input error");
+        }
+        System.out.println("***METHOD 2 END ***");
+        sc.close();
+    }
+}
