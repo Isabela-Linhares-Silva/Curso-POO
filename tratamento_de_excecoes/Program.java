06aec00e63921508e0ba3d0173017dcfe719f141
Isabela (Dev Local) <isabelalinharessilva@gmail.com>
2025-11-12 14:49:06 -0300

    uso do finally

---
+++ tratamento_de_excecoes/Program.java
@@ -0,0 +1,27 @@
+package tratamento_de_excecoes;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        File file = new File("C:\\temp\\in.txt");
+        Scanner sc = null;
+
+        try{
+            sc = new Scanner(file);
+            while (sc.hasNextLine()) {
+                System.out.println(sc.nextLine());
+            }
+        }
+        catch(FileNotFoundException e){
+            System.out.println("Error opening file: "+e.getMessage());
+        }
+        finally{
+            if (sc != null) {
+                sc.close(); 
+            }
+        }
+    }
+}
