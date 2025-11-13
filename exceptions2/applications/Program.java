d6b894d2276d6e427ccde1a17191b971dcd46a63
Isabela (Dev Local) <isabelalinharessilva@gmail.com>
2025-11-12 21:16:03 -0300

    solução ruim

---
+++ exceptions2/applications/Program.java
@@ -0,0 +1,49 @@
+package exceptions2.applications;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+import exceptions2.entities.Reservation;
+// metodo ruim para tratamento de excessoes
+public class Program {
+    public static void main(String[] args) throws ParseException {
+        Scanner sc = new Scanner(System.in);
+
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+        System.out.print("Room number: ");
+        int number = sc.nextInt();
+        System.out.print("Check-in date (dd/MM/yyyy): ");
+        Date ci = sdf.parse(sc.next());// parse pode gerar uma excessão, ou trata ou propaga
+        System.out.print("Check-out date (dd/MM/yyyy): ");
+        Date co = sdf.parse(sc.next());
+
+        if (!co.after(ci)) {
+            System.out.println("Error in reservation: Check-out date must be after check-in date");
+        }
+        else{
+            Reservation reservation = new Reservation(number, ci, co);
+            System.out.println("Reservation: "+reservation);
+
+            System.out.println();
+            System.out.println("Enter date to update the reservation: ");
+            System.out.print("Check-in date (dd/MM/yyyy):");
+            ci = sdf.parse(sc.next());
+            System.out.print("Check-out date (dd/MM/yyyy):");
+            co = sdf.parse(sc.next());
+           
+            String error = reservation.updateDates(ci, co);
+            if (error != null) {
+                System.out.println("Error in reser vation: "+ error);
+            }
+            else{
+                System.out.println("Reservation: "+reservation);
+            }
+                     
+        }
+        
+        sc.close();
+    }
+}
